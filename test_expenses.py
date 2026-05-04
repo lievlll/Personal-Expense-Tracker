3ea20ccf8a55c41ebda4a6a7b141f82f4475d4ae
lievlll <anfaloway@gmail.com>
2026-05-04 22:06:20 +0300

    Create test_expenses.py

---
+++ test_expenses.py
@@ -0,0 +1,26 @@
+import unittest
+from services.expense_service import ExpenseService
+from models.expense import Expense
+
+class TestExpenseService(unittest.TestCase):
+
+    def test_add(self):
+        s = ExpenseService()
+        s.add_expense(Expense(1, 100, "food", "2026-01-01", "test"))
+        self.assertEqual(len(s.expenses), 1)
+
+    def test_total(self):
+        s = ExpenseService()
+        s.add_expense(Expense(1, 100, "food", "2026-01-01", "a"))
+        s.add_expense(Expense(2, 50, "food", "2026-01-01", "b"))
+        self.assertEqual(s.get_total(), 150)
+
+    def test_delete(self):
+        s = ExpenseService()
+        e = Expense(1, 100, "food", "2026-01-01", "a")
+        s.add_expense(e)
+        s.delete_expense(1)
+        self.assertEqual(len(s.expenses), 0)
+
+if __name__ == "__main__":
+    unittest.main()
