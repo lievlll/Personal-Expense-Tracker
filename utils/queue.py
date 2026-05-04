0ae1a61668b3a392ed87c1013d0721f9ffd327e4
lievlll <anfaloway@gmail.com>
2026-05-04 22:07:41 +0300

    Create queue.py

---
+++ utils/queue.py
@@ -0,0 +1,9 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0) if self.items else None
